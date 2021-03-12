fc16a49af207b8fb6683a36da8e4af967e59887c
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2021-03-12 08:52:42 -0300

    removido region duplicate

--- values.tf
+++ values.tf
@@ -28,10 +28,10 @@ variable "function_name" {
   
 }
 
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
+# variable "aws_region" {
+#   type    = string
+#   default = "us-east-1"
+# }
 
 variable "environment_variables" {
   default     = {}
